4818bc56ed4ed621adfd3b8b8de977c4ee795ec7
RismusDrake <54723249+RismusDrake@users.noreply.github.com>
2019-12-01 22:18:45 +0300

    Delete config.py

--- Bot/data/config.py
+++
@@ -1,7 +0,0 @@
-bot_api = '1057831237:AAF5Qvkcz89Xot2k4s0Z5Mn907t3fESghKA'
-
-weather_api = 'cacb5e0c352d6a65e04b76c87d2a46da'
-
-url_movie = 'https://afisha.tut.by/film/'
-
-url_money = 'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0'
